44f6e79dc202d8c2aef1e4f4f84ee629cc2e6d66
jdv2357 <jdv2357@rit.edu>
2025-03-19 18:28:30 -0400

    added maven files, testing to import jsch

---
+++ fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/PostgresSSH.java
@@ -0,0 +1,61 @@
+import com.jcraft.jsch.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class PostgresSSH {
+    public static void main(String[] args) throws SQLException {
+
+        int lport = 5432;
+        String rhost = "starbug.cs.rit.edu";
+        int rport = 5432;
+        String user = "YOUR_CS_USERNAME"; //change to your username
+        String password = "YOUR_CS_PASSWORD"; //change to your password
+        String databaseName = "YOUR_DB_NAME"; //change to your database name
+
+        String driverName = "org.postgresql.Driver";
+        Connection conn = null;
+        Session session = null;
+        try {
+            java.util.Properties config = new java.util.Properties();
+            config.put("StrictHostKeyChecking", "no");
+            JSch jsch = new JSch();
+            session = jsch.getSession(user, rhost, 22);
+            session.setPassword(password);
+            session.setConfig(config);
+            session.setConfig("PreferredAuthentications","publickey,keyboard-interactive,password");
+            session.connect();
+            System.out.println("Connected");
+            int assigned_port = session.setPortForwardingL(lport, "127.0.0.1", rport);
+            System.out.println("Port Forwarded");
+
+            // Assigned port could be different from 5432 but rarely happens
+            String url = "jdbc:postgresql://127.0.0.1:"+ assigned_port + "/" + databaseName;
+
+            System.out.println("database Url: " + url);
+            Properties props = new Properties();
+            props.put("user", user);
+            props.put("password", password);
+
+            Class.forName(driverName);
+            conn = DriverManager.getConnection(url, props);
+            System.out.println("Database connection established");
+
+            // Do something with the database....
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Closing Database Connection");
+                conn.close();
+            }
+            if (session != null && session.isConnected()) {
+                System.out.println("Closing SSH Connection");
+                session.disconnect();
+            }
+        }
+    }
+}
