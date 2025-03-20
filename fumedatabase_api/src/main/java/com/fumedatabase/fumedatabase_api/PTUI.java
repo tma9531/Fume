92292c9c54897a49b03a32444f939c46637a8583
Aron Lee <ajl4541@rit.edu>
2025-03-20 12:09:29 -0400

    cleaned up PTUI code

--- fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/PTUI.java
+++ fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/PTUI.java
@@ -1,59 +1,48 @@
 package com.fumedatabase.fumedatabase_api;
 
-import java.sql.SQLException;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.SQLException;
 
 import com.jcraft.jsch.JSchException;
 
-
 public class PTUI {
+
+    final static String LOGIN_DETAILS_PATH = "../../../../../../docs/login.txt";
+
     public static void main(String[] args) throws SQLException {
+        SSHConnectionManager sshManager = new SSHConnectionManager();
+        DatabaseConnectionManager dbManager = new DatabaseConnectionManager();
         try {
-            FileReader read = new FileReader("/login.txt");
+            FileReader read = new FileReader(LOGIN_DETAILS_PATH);
             BufferedReader br = new BufferedReader(read);
-            Scanner scanner = new Scanner(System.in);
+            String user = br.readLine().trim();
+            String password = br.readLine().trim();
 
-            String user = br.readLine().trim(); // Read username from file
-            String password = br.readLine().trim(); // Read password from file
-            SSHConnectionManager sshManager = new SSHConnectionManager();
-            DatabaseConnectionManager dbManager = new DatabaseConnectionManager();
-
-            // Establish SSH connection and port forwarding
             sshManager.connect(user, password);
-            
-            // Establish database connection
             dbManager.connect(user, password);
             
-            // Get the database connection
             var conn = dbManager.getConnection();
             if (conn != null) {
                 System.out.println("Database connection established successfully.");
-                
-                // Create and execute queries
                 Queries queries = new Queries(conn);
                 queries.executeQueries();
             } else {
                 System.err.println("Failed to establish database connection.");
             }
-
+            
+            br.close();
+            read.close();
         } catch (JSchException e) {
             System.out.println("Bad Credentials");
         } 
         catch (Exception e) {
+            System.out.println("SSH Connection failed.");
             e.printStackTrace();
         }
         finally {
-            try{
-                // Disconnect from the database and SSH session
-                dbManager.disconnect();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } finally {
-                sshManager.disconnect();
-                scanner.close();
-            }
+            dbManager.disconnect();
+            sshManager.disconnect();
         }
     }
 }
