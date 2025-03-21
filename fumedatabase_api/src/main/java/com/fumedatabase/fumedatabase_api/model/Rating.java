6dd8c9a06b7fb3a6a491dc6a878e53c5624e8222
Emmett Peterson <ejp1931@rit.edu>
2025-03-21 17:37:23 -0400

    added follow and unfollow functions to user class in addition to adding rating class and functionality

---
+++ fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/model/Rating.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.fumedatabase.fumedatabase_api.model;
+
+/**
+ *
+ * @author emmettpeterson
+ */
+public class Rating {
+
+}
