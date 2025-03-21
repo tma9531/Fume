a381db9031c6811431c566a2f312cefcace05c4a
Emmett Peterson <ejp1931@rit.edu>
2025-03-21 17:23:09 -0400

    rating added

--- fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/model/Platform.java
+++ fumedatabase_api/src/main/java/com/fumedatabase/fumedatabase_api/model/Platform.java
@@ -9,6 +9,32 @@ package com.fumedatabase.fumedatabase_api.model;
  *
  * @author emmettpeterson
  */
-public class Platform.java {
+public class Platform {
+
+    private String name;
+    private int pnr;
+
+    public Platform(String name) {
+        this.name = name;
+    }
+
+    public Platform(int pnr, String name) {
+        this.pnr = pnr;
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPnr(){
+        return pnr;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    
 
 }
